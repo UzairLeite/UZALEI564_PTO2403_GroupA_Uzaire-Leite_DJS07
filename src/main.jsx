b6c3632ebac0d69b68334914e4d7af8ad2f662a8
UzairLeite <102726763+UzairLeite@users.noreply.github.com>
2025-01-23 00:40:34 +0200

    set up initial scripts

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import React from "react"
+import Header from "./components/Header"
+import Meme from "./components/Meme"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
+export default function App() {
+    return (
+        <div>
+            <Header />
+        </div>
+    )
+}
