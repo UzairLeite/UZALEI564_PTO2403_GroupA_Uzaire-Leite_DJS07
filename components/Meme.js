162d964725f2cdf351d34a8e6babae0e577f19af
UzairLeite <102726763+UzairLeite@users.noreply.github.com>
2025-01-23 22:57:11 +0200

    Update meme.js script

--- components/Meme.js
+++ components/Meme.js
@@ -4,17 +4,25 @@ export default function Meme() {
     return (
         <main>
             <form className="form">
-                <input 
-                    type="text"
-                    placeholder="Top text"
-                    className="form--input"
-                />
-                <input 
-                    type="text"
-                    placeholder="Bottom text"
-                    className="form--input"
-                />
-                <button 
+                <div>
+                    <label htmlFor="top-text">Top Text</label>
+                    <input
+                        id="top-text"
+                        type="text"
+                        placeholder="Shut up"
+                        className="form--input"
+                    />
+                </div>
+                <div>
+                    <label>Bottom Text
+                    <input
+                            type="text"
+                            placeholder="and take my money"
+                            className="form--input"
+                        />
+                    </label>
+                </div>
+                <button
                     className="form--button"
                 >
                     Get a new meme image 🖼
