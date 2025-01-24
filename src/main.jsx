50142e1b06bb30ad1fb021fcebbdf49267159a23
UzairLeite <102726763+UzairLeite@users.noreply.github.com>
2025-01-24 23:40:05 +0200

    Completed Memes.JS data

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import React from "react"
-import Header from "./components/Header"
-import Meme from "./components/Meme"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
